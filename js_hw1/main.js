fc5e38e094d01cbb400baad150a1b2dbfe70e498
fabmansik <milansomyk@gmail.com>
2023-05-23 18:52:59 +0300

    JS lesson 1 - index.html - main.js - js_hw1.rar

---
+++ js_hw1/main.js
@@ -0,0 +1,34 @@
+let n='hello'
+let o='owu'
+let p='com'
+let d='ua'
+let e=1
+let f=10
+let g=-999
+let h=123
+let i=3.14
+let j=2.7
+let k=16
+let l=true
+let m=false
+
+console.log(n,o,p,d,e,f,g,h,i,j,k,l,m)
+
+let firstName = 'Milan'
+let middleName='Mykhailovich'
+let lastName='Somyk'
+let person = `${firstName} ${middleName} ${lastName}`
+console.log(person)
+
+let a=100
+let b='100'
+let c=true
+console.log(typeof a,b,c)
+
+let name= prompt("Вкажіть ім'я:")
+let secondName = prompt("Вкажіть по батькові:")
+let age = prompt("Вкажіть вік:")
+age = +age
+console.log(name, secondName, age)
+
+console.log( typeof age)
